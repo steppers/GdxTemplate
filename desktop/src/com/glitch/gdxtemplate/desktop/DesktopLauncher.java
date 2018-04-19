4b3baf28308419ddff9160a3e2490bb1fd7e93b5
Oliver Steptoe <osteptoe@wegener.local>
2018-04-19 16:52:53 +0100

    add template

---
+++ desktop/src/com/glitch/gdxtemplate/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.glitch.gdxtemplate.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.glitch.gdxtemplate.GdxTemplate;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GdxTemplate(), config);
+	}
+}
---
+++ ios/src/com/glitch/gdxtemplate/IOSLauncher.java
@@ -0,0 +1,22 @@
+package com.glitch.gdxtemplate;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.glitch.gdxtemplate.GdxTemplate;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new GdxTemplate(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
